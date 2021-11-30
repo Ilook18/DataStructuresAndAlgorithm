d83d082f4df7bc94383749c5ae5887c57844f1fc
“Enter <anirudhanand984@gmail.com>
2021-11-30 18:20:30 +0530

    [Anirudh] add : created unordered List

---
+++ DataStructuresAndAlgorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DataStructuresAlgorithm.Utility;
+
+namespace DataStructuresAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE OPTION : 1.Unordered List\n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UnOrderedList unorder = new UnOrderedList();
+                        unorder.WordSearch(@"C:\Users\DELL\source\repos\DataStructuresAndAlgorithm\Utility\Word.txt");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    
+                }
+            }
+
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithm/UnorderedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructuresAlgorithm.Utility;
+
+namespace DataStructuresAlgorithm.Utility
+{
+    class UnOrderedList
+    {
+        LinkedList<string> linkedList = new LinkedList<string>();
+
+        public void WordSearch(string WordFilepath)
+        {
+            string data = File.ReadAllText(WordFilepath);
+            string[] words = data.Split(" ");
+            foreach (var result in words)
+            {
+                linkedList.Add(result);
+
+            }
+            Console.WriteLine("enter the word you want to search for");
+            string word = Console.ReadLine();
+            if (linkedList.Search(word))
+            {
+                linkedList.Delete(word);
+            }
+            else
+            {
+                linkedList.Add(word);
+            }
+            linkedList.Display();
+            string text = linkedList.Write();
+            File.WriteAllText(WordFilepath, text);
+        }
+    }
+}
