bf2fb50eb3abca7b155d4f82454a63803bbfbae4
“Enter <anirudhanand984@gmail.com>
2021-11-30 19:28:10 +0530

    [Anirudh] add : created Banking Cash counter program

--- DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/Program.cs
@@ -10,7 +10,7 @@ namespace DataStructuresAlgorithm
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("CHOOSE OPTION : 1.Unordered List\n2.OrderedList\n 3.BalancedParanthesis\n 4.Exit");
+                Console.WriteLine("CHOOSE OPTION : 1.Unordered List\n2.OrderedList\n 3.BalancedParanthesis\n 4.CashCounter 5.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -27,6 +27,10 @@ namespace DataStructuresAlgorithm
                         Bal.ParanthsisCheck(@"C:\Users\DELL\source\repos\DataStructuresAndAlgorithm\Utility\parenthesis.txt");
                         break;
                     case 4:
+                        CashCounter cash= new CashCounter();
+                        cash.CashManager();
+                        break;
+                    case 5:
                         flag = false;
                         break;
 
