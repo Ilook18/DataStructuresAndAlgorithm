6d86fcb46ccc9e721b277f04556846edf275e850
“Enter <anirudhanand984@gmail.com>
2021-11-30 18:58:50 +0530

    [Anirudh] add : created Balanced Parenthesis program

---
+++ DataStructuresAndAlgorithm/BalancedParenthesis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAlgorithm.Utility
+{
+    class BalancedParenthesis
+    {
+
+        StackOperation<string> stack = new StackOperation<string>();
+        public void ParanthsisCheck(string BalFilePath)
+        {
+            string text = File.ReadAllText(BalFilePath);
+            string[] par = text.Split(" ");
+            foreach (var item in par)
+            {
+                if (item == "(")
+                {
+                    stack.Push("(");
+                }
+                if (item == ")")
+                {
+                    stack.Pop(")");
+                }
+            }
+            if (stack.IsEmpty())
+            {
+                Console.WriteLine("This is a Balanced Prenthesis");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Balanced Parenthesis");
+            }
+        }
+    }
+}
--- DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/Program.cs
@@ -10,7 +10,7 @@ namespace DataStructuresAlgorithm
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("CHOOSE OPTION : 1.Unordered List\n 2.OrderedList\n 3.Exit");
+                Console.WriteLine("CHOOSE OPTION : 1.Unordered List\n2.OrderedList\n 3.BalancedParanthesis\n 4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -23,9 +23,13 @@ namespace DataStructuresAlgorithm
                         order.Numbers(@"c:\users\dell\source\repos\datastructuresandalgorithm\Utility\Numbers.txt");
                         break;
                     case 3:
+                        BalancedParenthesis Bal = new BalancedParenthesis();
+                        Bal.ParanthsisCheck(@"C:\Users\DELL\source\repos\DataStructuresAndAlgorithm\Utility\parenthesis.txt");
+                        break;
+                    case 4:
                         flag = false;
                         break;
-                    
+
                 }
             }
 
